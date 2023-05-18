40a78ebfbd1612484dda9ab0d3fa96edb22956c4
Fabian <fabian.schoedel@gmx.de>
2023-05-18 08:00:41 +0200

    Tilepalette, Script Rework

--- Dragon Slayer/Assets/Scripts/Player/MovementScript.cs
+++ Dragon Slayer/Assets/Scripts/Player/MovementScript.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class MovementScript : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Player;
 
-    public float speed;
+    public float Speed;
 
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        player.transform.Translate(new Vector2(horizontalInput, verticalInput) * speed * Time.deltaTime);
+        Player.transform.Translate(new Vector2(horizontalInput, verticalInput) * Speed * Time.deltaTime);
     }
 }
