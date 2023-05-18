9b3eeec4624d94efb3dcd5eb5a5e6f0c1e0579e4
Fabian <fabian.schoedel@gmx.de>
2023-05-18 10:34:06 +0200

    Damage System implement

--- Dragon Slayer/Assets/Scripts/Player/DamageScript.cs
+++ Dragon Slayer/Assets/Scripts/Player/DamageScript.cs
@@ -1,15 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DamageScript : MonoBehaviour
 {
+    public int Health;
+    public int Maxhealth;
+    public int Damage;
+
+    public Image HealthBar;
+    public GameObject Player;
+
+    private void Start()
+    {
+        Health = Maxhealth;
+    }
+
+    private void Update()
+    {
+        //HealthBar.fillAmount = Health / Maxhealth;
+
+        if (Health <= 0)
+        {
+            Destroy(Player);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            GetComponent<Player>().Health -= 40;
+            Health -= Damage;
+
+            print(Health);
         }
     }
 }
--- Dragon Slayer/Assets/Scripts/Player/Player.cs
+++ Dragon Slayer/Assets/Scripts/Player/Player.cs
@@ -5,27 +5,5 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int MaxHealth;
 
-    int _Health;
-    public int Health
-    {
-        get => _Health;
-
-        set
-        {
-            if (value <= 0)
-            {
-                print("dead");
-                Destroy(gameObject.transform.parent);
-            }
-
-            _Health = value;
-        }
-    }
-
-    private void Start()
-    {
-        Health = MaxHealth;
-    }
 }
